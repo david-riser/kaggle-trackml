de97b09a6f3bf2203b066f1bfe1075b5b484e246
David Riser <david.riser@uconn.edu>
2018-07-27 18:15:31 -0400

    commit message here

--- src/models/1.1-predict-histogram-clustering.py
+++ src/models/1.1-predict-histogram-clustering.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #
-# 1.0-predict-histogram-clustering.py
+# 1.1-predict-histogram-clustering.py
 #
 # Author: David Riser
 # Date:   July 27, 2018
@@ -12,6 +12,9 @@
 # This algorithm uses the best features
 # from previous works and is written for
 # speed not for generalization.
+#
+# In this 1.1 version, I introduce ensembling
+# of different unrolled labels. 
 
 import numpy as np
 import pandas as pd
@@ -87,32 +90,60 @@ def test_events():
     ''' Process a few events to demonstrate the method
     before using it on the entire testing dataset.
     '''
-    events        = range(1000, 1012)
+    events        = range(1000, 1005)
     path_to_train = '../../data/raw/train_100_events/'
     parts_to_load = ['hits', 'truth']
 
-    model = Clusterer(350, 2, 40, 40)
+    model = Clusterer(800, 2, 40, 40)
     for event_id in events:
 
         with timer('Processing %s' % event_id):
             hits, truth = load_event(path_to_train+'event00000'+str(event_id), parts=parts_to_load)
 
-            # Add the needed information.
+            # Setup the parameters
+            dz0     = -0.00070
+            dz_step = 0.00001
+
             r = np.sqrt(hits['x'].values ** 2 + hits['y'].values ** 2)
             d = np.sqrt(hits['x'].values ** 2 + hits['y'].values ** 2 + hits['z'].values**2)
-            hits['phi'] = np.arctan2(hits['y'].values, hits['x'].values)
-            hits['sphi'] = np.sin(hits['phi'].values)
-            hits['cphi'] = np.cos(hits['phi'].values)
-            hits['zd'] = hits['z'].values / d
-            hits['zr'] = hits['z'].values / r
+            hits['zd']   = hits['z'].values / d
+            hits['zr']   = hits['z'].values / r
 
-            scaler = MinMaxScaler()
-            X = scaler.fit_transform(hits[['zr', 'zd', 'sphi', 'cphi']])
+            labels = []
+            counts = []
+            for i in range(0, 100):
+                dz = dz0 + i*dz_step
 
-            labels = model.predict(X)
-            submission = create_one_event_submission(event_id, hits, labels)
+                # Add the needed information.
+                hits['phi']  = np.arctan2(hits['y'].values, hits['x'].values)
+                phi_prime = hits['phi'].values + np.sign(hits['z'].values) * \
+                            dz * hits['z'].values
+
+                hits['sphi'] = np.sin(phi_prime)
+                hits['cphi'] = np.cos(phi_prime)
+
+                scaler = MinMaxScaler()
+                X = scaler.fit_transform(hits[['zr', 'zd', 'sphi', 'cphi']])
+                
+                labels.append(model.predict(X))
+                unique, reverse, count = np.unique(labels[i], return_counts=True, return_inverse=True)
+                count = count[reverse]
+                count[np.where(labels[i] == 0)] = 0
+                count[np.where(labels[i] > 20)] = 0
+                counts.append(count)
+                
+
+            final_labels, final_counts = labels[0], counts[0]
+            for l, c in zip(labels[1:], counts[1:]):
+                idx = np.where((c-final_counts > 0) & (l!=0))[0]
+                final_labels[idx] = l[idx] + final_labels.max()
+                final_counts[idx] = c[idx]
+
+            # Submission 
+            submission = create_one_event_submission(event_id, hits, final_labels)
             score = score_event_fast(truth, submission)
             print('score = %.4f' % score)
 
+                
 if __name__ == '__main__':
     test_events()
